86646c58840c312d3abfe98ba3fc5b23b403d8d8
GABRIEL ENRIQUE SOLAMAYO MUNOZ <gabriel.esmunoz@senacsp.edu.br>
2022-09-21 11:26:24 -0300

    Aula Estrutura de Dados (21/09/2022) - Só anotações

---
+++ 3_Semestre/Estrutura de Dados/Estrutura_de_Dados_Projeto/src/Aula_4/MergeSort.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula_4;
+
+import java.util.Random;
+/**
+ *
+ * @author gabriel.esmunoz
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        float vetor[] = new float[6];
+        Random rd = new Random();
+        int vi= 0, vf=11;
+        vetor[0] = vi+rd.nextInt(vf-vi);
+        
+        
+    }
+}
