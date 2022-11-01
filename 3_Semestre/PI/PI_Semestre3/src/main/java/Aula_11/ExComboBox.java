ab70798677c5e3b1565495d70aec30bd2db4919f
GABRIEL ENRIQUE SOLAMAYO MUNOZ <gabriel.esmunoz@senacsp.edu.br>
2022-11-01 11:26:56 -0300

    Upando o resto da aula 11

--- 3_Semestre/PI/PI_Semestre3/src/main/java/Aula_11/ExComboBox.java
+++ 3_Semestre/PI/PI_Semestre3/src/main/java/Aula_11/ExComboBox.java
@@ -13,36 +13,67 @@ import java.awt.event.*;
  */
 public class ExComboBox extends JFrame implements ActionListener, ItemListener{
     
-    JLabel l1;
-    JTextField t1, t2, t3;
-    JComboBox combo;
-    JButton b1, b2, b3, b4, b5, b6; //Instanciações
-    
-    public ExComboBox (){
-        setTitle("Uso do JComboBox");
-        setSize(400, 170);
-        getContentPane().setBackground(new Color(190, 190, 190));
-        l1 = new JLabel("Conteudo");
-        l1.setHorizontalAlignment(SwingConstants.CENTER);
-        l1.setForeground(Color.blue);
-        l1.setFont(new Font("Arial", Font.BOLD, 15));
-        t1 = new JTextField();
-        t2 = new JTextField();
-        t1.setHorizontalAlignment(SwingConstants.CENTER);
-        t2.setHorizontalAlignment(SwingConstants.CENTER);
-        String cores[] = {"Branco", "Vermelho","Azul", "Verde"};
-        }
-    
-    private JButton criarBotao(String texto, int x, int y, int w, int h, char c) {
-        JButton b1 = new JButton(texto);
-        b1.setBounds(x, y, w, h);
-        b1.setFont(new Font("Helvetica", Font.BOLD, 18));
-//        b1.setToolTipText("Botao b1");
-        //b1.addActionListener(this);
-        b1.setHorizontalAlignment(SwingConstants.CENTER);
-        b1.setVerticalAlignment(SwingConstants.CENTER);
-        b1.setMnemonic(c); 
-        add(b1);
-        return b1;
-    }
+	JLabel l1;
+	JTextField t1,t2,t3;
+	JComboBox combo;
+	JButton b1,b2,b3,b4,b5,b6; //instanciações
+	public static void main(String args[])	{
+		new ExComboBox();   
+	}
+	ExComboBox ()
+	{
+		setTitle("Uso do JComboBox");
+		setSize(400,170); 	
+		getContentPane().setBackground(new Color(135,148,245)); 
+		l1 = new JLabel("Conteudo");
+		l1.setHorizontalAlignment(SwingConstants.CENTER);
+		l1.setForeground(Color.blue);
+		l1.setFont(new Font("Arial", Font.BOLD, 15)); 
+		t1 = new JTextField(); t2 = new JTextField(); 
+		t1.setHorizontalAlignment(SwingConstants.CENTER);        
+		t2.setHorizontalAlignment(SwingConstants.CENTER);
+		String[] cores = {"Branco","Vermelho","Azul","Verde"}; 
+		combo = new JComboBox(cores); 
+		combo.addItemListener(this); 
+		getContentPane().setLayout(new GridLayout(5,2)); //5 linhas e 2 colunas
+		getContentPane().add(l1);	
+		getContentPane().add(combo);
+		b1 = criarBotao("Mostra Texto");	
+		b4 = criarBotao("Remove Item");     
+		b2 = criarBotao("Mostra Índice");	
+		b5 = criarBotao("Remove Todos");    
+		b3 = criarBotao("Adiciona Item");   	
+		getContentPane().add(t1);  
+		b6 = criarBotao("Quant. Itens");	
+		getContentPane().add(t2);  
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	private JButton criarBotao(String texto) {
+		JButton botao = new JButton(texto);
+		botao.addActionListener(this);
+		add(botao);
+		return botao;
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource()==b1)
+			l1.setText("Texto: "+combo.getSelectedItem());
+		if (e.getSource()==b2)
+			l1.setText("Índice: " + combo.getSelectedIndex()); 
+		if (e.getSource()==b3)
+			if (t1.getText().length()!=0)
+			{combo.addItem(t1.getText());//adiciona item
+			t1.setText("");      }//limpa o texto de t1 
+		if (e.getSource()==b4)
+			combo.removeItemAt(combo.getSelectedIndex());// remove o item selecionado
+		if (e.getSource()==b5)
+			combo.removeAllItems();//remove todos itens 
+		if (e.getSource()==b6) 
+			t2.setText(""+combo.getItemCount());//conta a quantidade total de itens     
+	}
+	public void itemStateChanged(ItemEvent e)//trata a seleção no combo
+	{ 
+		t1.setText(""+combo.getSelectedItem());//mostra o item selecionado  
+	} 
 }
